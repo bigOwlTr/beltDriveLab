3dac38e339fcc6e5136bdd9aede5138555a30f53
Colin Peden <colin.p.peden@gmail.com>
2025-03-10 18:44:36 +0000

    now plots tension for all 4 to complete part one of resutls

--- main.m
+++ main.m
@@ -1,7 +1,22 @@
-clear;close all;clc;       %clearing the workspace and any windows
+clear; close all; clc;
 
 data90 = readtable("data.xlsx", 'Range', 'C3:F12');
 data180 = readtable("data.xlsx", 'Range', 'C13:F22');
 data270 = readtable("data.xlsx", 'Range', 'I3:T12');
 data360 = readtable("data.xlsx", 'Range', 'C23:F32');
 
+figure('Units', 'centimeters', 'Position', [3, 3, 16, 10]);
+
+hold on;
+
+plot(data90{:,4}, data90{:,3}, 'LineWidth', 2, 'DisplayName', '90°');
+plot(data180{:,4}, data180{:,3}, 'LineWidth', 2, 'DisplayName', '180°');
+plot(data270{:,4}, data270{:,3}, 'LineWidth', 2, 'DisplayName', '270°');
+plot(data360{:,4}, data360{:,3}, 'LineWidth', 2, 'DisplayName', '360°');
+
+xlabel('T_2 (N)', 'FontSize', 14, 'FontName', 'Arial');
+ylabel('T_1 (N)', 'FontSize', 14, 'FontName', 'Arial');
+h_legend = legend('Location', 'southeast');
+set(h_legend, 'FontSize', 14);  % Increase legend font size
+grid on;
+set(gca, 'FontSize', 12, 'LineWidth', 1.5);
