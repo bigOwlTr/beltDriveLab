b908071c57aac422f73f356db49dd2b8a9504669
Colin Peden <colin.p.peden@gmail.com>
2025-03-12 23:17:08 +0000

    more consistent formatting

--- main.m
+++ main.m
@@ -13,13 +13,13 @@ hold on;
 
 
 scatter(data90{:,3}, data90{:,4}, 50, 'b', 'DisplayName', ...
-    '90° Data', 'Marker', '^');
+    '90° Data', 'Marker', '^', 'LineWidth', 1.5);
 scatter(data180{:,3}, data180{:,4}, 50, 'r', 'DisplayName', ...
-    '180° Data', 'Marker', '^');
+    '180° Data', 'Marker', '^', 'LineWidth', 1.5);
 scatter(data270{:,3}, data270{:,4}, 50, 'g', 'DisplayName', ...
-    '270° Data', 'Marker', '^');
+    '270° Data', 'Marker', '^', 'LineWidth', 1.5);
 scatter(data360{:,3}, data360{:,4}, 50, 'k', 'DisplayName', ...
-    '360° Data', 'Marker', '^');
+    '360° Data', 'Marker', '^', 'LineWidth', 1.5);
 
 
 mdl90 = fitlm(data90{:,3}, data90{:,4});
@@ -28,13 +28,13 @@ mdl270 = fitlm(data270{:,3}, data270{:,4});
 mdl360 = fitlm(data360{:,3}, data360{:,4});
 
 
-plot(data90{:,3}, predict(mdl90, data90{:,3}), 'b:', 'LineWidth', 1, ...
+plot(data90{:,3}, predict(mdl90, data90{:,3}), 'b:', 'LineWidth', 1.5, ...
     'DisplayName', '90° Linear Fit');
-plot(data180{:,3}, predict(mdl180, data180{:,3}), 'r:', 'LineWidth', 1, ...
+plot(data180{:,3}, predict(mdl180, data180{:,3}), 'r:', 'LineWidth', 1.5, ...
     'DisplayName', '180° Linear Fit');
-plot(data270{:,3}, predict(mdl270, data270{:,3}), 'g:', 'LineWidth', 1, ...
+plot(data270{:,3}, predict(mdl270, data270{:,3}), 'g:', 'LineWidth', 1.5, ...
     'DisplayName', '270° Linear Fit');
-plot(data360{:,3}, predict(mdl360, data360{:,3}), 'k:', 'LineWidth', 1, ...
+plot(data360{:,3}, predict(mdl360, data360{:,3}), 'k:', 'LineWidth', 1.5, ...
     'DisplayName', '360° Linear Fit');
 
 xlabel('T_2 (N)', 'FontSize', 12);
@@ -42,6 +42,8 @@ ylabel('T_1 (N)', 'FontSize', 12);
 h_legend = legend('Location', 'northwest');
 set(h_legend, 'FontSize', 10);
 grid on;
+
+set(gca, 'FontSize', 12, 'LineWidth', 1.5);
 
 hold off;
 
@@ -60,8 +62,8 @@ mubeta_values = [mubeta90, mubeta180, mubeta270, mubeta360];
 
 figure('Units', 'centimeters', 'Position', [3, 3, 16, 10]);
 
-h1 = scatter(angles_rad, mubeta_values, 100, 'Marker', '^', ...
-    'LineWidth', 2, 'MarkerEdgeColor', 'b');
+h1 = scatter(angles_rad, mubeta_values, 60, 'Marker', '^', ...
+    'LineWidth', 1.5, 'MarkerEdgeColor', 'b');
 
 mdl = fitlm(angles_rad, mubeta_values);
 
@@ -69,26 +71,27 @@ hold on;
 x_values = linspace(min(angles_rad), max(angles_rad), 100);
 y_values = predict(mdl, x_values');
 
-h2 = plot(x_values, y_values, 'b:', 'LineWidth', 2);
+h2 = plot(x_values, y_values, 'b:', 'LineWidth', 1.5);
 
 xlabel('\beta (Radians)', 'FontSize', 12);
-ylabel('log(T_2/T_1)', 'FontSize', 12);
+ylabel('ln(T_2/T_1)', 'FontSize', 12);
 
 equation_text = sprintf('y = %.2fx + %.2f', mdl.Coefficients.Estimate(2) ...
     , mdl.Coefficients.Estimate(1));
-text(angles_rad(2), mubeta_values(2), equation_text, 'FontSize', 12, ...
+text(angles_rad(2), mubeta_values(2), equation_text, 'FontSize', 10, ...
     'VerticalAlignment', 'top');
 
 legend([h1, h2], {'Data points', 'Linear fit'}, 'Location', 'northwest', ...
-    'FontSize', 12);
+    'FontSize', 10);
 
 grid on;
 
 xlim([0, 2*pi]);
-ylim([min(mubeta_values)-1, max(mubeta_values)+1]);
+ylim([0, max(mubeta_values)+1]);
 
 set(gca, 'FontSize', 12, 'LineWidth', 1.5);
 hold off;
+
 
 %-------------------------------predicted tension ratio--------------------
 theta = linspace(0, 2*pi, 10); 
